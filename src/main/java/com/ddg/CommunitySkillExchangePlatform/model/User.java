5a0d23aba0f3d802d304296e41a820dd7085de49
gaddganepola <ganeypola@gmail.com>
2025-08-10 11:06:56 +0530

    create the user model

---
+++ src/main/java/com/ddg/CommunitySkillExchangePlatform/model/User.java
@@ -0,0 +1,27 @@
+package com.ddg.CommunitySkillExchangePlatform.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
+
+enum Role {
+    USER,
+    ADMIN
+}
