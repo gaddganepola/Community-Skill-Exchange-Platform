33fcb2ed1d53ee26d6ff60dc24c9d9995a49f8f6
gaddganepola <ganeypola@gmail.com>
2025-08-10 10:51:07 +0530

    initial commit

---
+++ src/main/java/com/ddg/CommunitySkillExchangePlatform/CommunitySkillExchangePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ddg.CommunitySkillExchangePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitySkillExchangePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitySkillExchangePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddg/CommunitySkillExchangePlatform/CommunitySkillExchangePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddg.CommunitySkillExchangePlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitySkillExchangePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
