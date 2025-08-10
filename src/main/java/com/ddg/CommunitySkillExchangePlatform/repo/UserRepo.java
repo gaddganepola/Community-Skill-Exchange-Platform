748ac980b13a5b54bad13873c16a7e969f048a2b
gaddganepola <ganeypola@gmail.com>
2025-08-10 11:18:11 +0530

    create the user repo

---
+++ src/main/java/com/ddg/CommunitySkillExchangePlatform/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.ddg.CommunitySkillExchangePlatform.repo;
+
+import com.ddg.CommunitySkillExchangePlatform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+}
